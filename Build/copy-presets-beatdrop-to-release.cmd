8545b22136aa4f13e53e96a50b4d4c35035a65eb
IkeC <king.of.all.cosmos@gmail.com>
2025-07-18 09:46:23 +0200

    updated preset upstream sync

--- Build/copy-presets-beatdrop-to-release.cmd
+++ Build/copy-presets-beatdrop-to-release.cmd
@@ -11,11 +11,19 @@ pause
 robocopy %SRCDIR% %DESTDIR%\BeatDrop /LEV:1
 robocopy "%SRCDIR%\Butterchurn Presets" "%DESTDIR%\Butterchurn"
 robocopy "%SRCDIR%\Incubo_'s Presets" "%DESTDIR%\Incubo_"
-robocopy "%SRCDIR%\Incubo_ Picks" "%DESTDIR%\Incubo_ Picks"
+robocopy "%SRCDIR%\Incubo_'s Picks" "%DESTDIR%\Incubo_ Picks"
 robocopy "%SRCDIR%\Milkdrop2077 Presets" "%DESTDIR%\Milkdrop2077"
 
-:: textures
-:: we should cherry-pick the required ones
-::robocopy %SRCDIR%\textures %DESTDIR%\..\textures
+::exclusions, often because preset is using big textures that would bloat the release
+cd "%DESTDIR%"
+SET CMD=del /s /q
+%CMD% "*Gillman*" "*Violetta*" "*Watery*" "*Grizelda*" "*Octobella*" "*Nerissa*" "*Spooky*" "*Abbie*" "*Chelsea*" "*Lucious*" "*Orticia*"
+%CMD% "*Dancing Saber*" "*BPM Code Test*" "*Bing AI*Try*"
+%CMD% "Aderrasi - Bitterfeld (Crystal Border Mix) - [Jian Simanjuntak with forest tree edit].milk"
+%CMD% "*Altars Of Madness 2 (Cold Snap Edit)*"
+pause
 
-pause 
+::textures
+::we should only cherry-pick the required ones
+::
+::robocopy %SRCDIR%\textures %DESTDIR%\..\textures
