f5f18e1018943b5fd43ac556428ce76cc73875cb
IkeC <king.of.all.cosmos@gmail.com>
2025-08-16 11:32:23 +0200

    v3 presets

--- Build/2-copy-presets-release-to-visualizer.cmd
+++ Build/2-copy-presets-release-to-visualizer.cmd
@@ -10,8 +10,9 @@ robocopy %SRCDIR%\Incubo_ %DESTDIR%\Incubo_
 robocopy %SRCDIR%\Incubo_ Picks %DESTDIR%\Incubo_ Picks
 robocopy %SRCDIR%\Milkdrop2077 %DESTDIR%\Milkdrop2077
 robocopy %SRCDIR%\Milkwave %DESTDIR%\Milkwave
+robocopy %SRCDIR%\Shader %DESTDIR%\Shader
 
 :: textures
 robocopy %SRCDIR%\..\textures %DESTDIR%\..\textures
 
-pause 
+pause
